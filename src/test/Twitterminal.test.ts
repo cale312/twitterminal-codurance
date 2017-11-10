f3cb65e208293475b7b9bf5dfe8296e8124c34e8
ggsbv <ggsbv@protonmail.com>
2017-11-10 20:39:11 +0200

    created test for Twitterminal Class for handling ReadCommands as input

--- src/test/Twitterminal.test.ts
+++ src/test/Twitterminal.test.ts
@@ -45,4 +45,27 @@ describe("The Twitterminal Class", () => {
             "This is my second post..."
         ])
     });
+
+    it(`should display the User's timeline when the input command's subject is only a user name`, () => {
+        let database = new Database();
+        let userRepository = new UserRepository();
+        let postRepository = new PostRepository();
+        let postCommand = new PostCommand();
+        let readCommand = new ReadCommand();
+
+        let availableCommands = [
+            postCommand,
+            readCommand
+        ];
+
+        let twitterminal = new Twitterminal(availableCommands, userRepository, postRepository);
+
+        twitterminal.handleInput("Sandro -> This is my first post!");
+        twitterminal.handleInput("Sandro -> This is my second post!");
+
+        assert.deepEqual(timeline.display(userRepository.findOne({ user: "Sandro" }).posts), [
+            "This is my second post!",
+            "This is my first post!"
+        ]);
+    });
 });
